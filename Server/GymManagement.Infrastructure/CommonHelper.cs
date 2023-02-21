4712e37470c40018bafce1d5b78aa5e0758b0b73
AndyLiang1 <andyliang1000@gmail.com>
2023-02-21 11:26:12 -0800

    BE compatible with SQL Server DB

--- Server/GymManagement.Infrastructure/CommonHelper.cs
+++ Server/GymManagement.Infrastructure/CommonHelper.cs
@@ -4,12 +4,8 @@ namespace GymManagement.Infrastructure;
 
 
 public class CommonHelper {
-    private IConfiguration _config;
-
-    public CommonHelper(IConfiguration config) {
-        _config = config;
-    }
-
     
+
+
 
 }
--- Server/GymManagement.Infrastructure/Persistence/UserRepository.cs
+++ Server/GymManagement.Infrastructure/Persistence/UserRepository.cs
@@ -1,12 +1,18 @@
 using GymManagement.Application.Persistence;
 using GymManagement.Domain.Entities;
 
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 namespace GymManagement.Infrastructure.Persistence;
 
 public class UserRepository : IUserRepository
 {
     private static readonly List<User> _users = new();
+    private IConfiguration _config;
 
+    public UserRepository(IConfiguration config) {
+        _config = config;
+    }
     public void Add(User user)
     {
         _users.Add(user);
@@ -18,6 +24,19 @@ public class UserRepository : IUserRepository
     }
 
     public List<User> GetAll () {
+        string connectionString = "Data Source=localhost;Initial Catalog=Tutorial2;Integrated Security=True";
+        using (SqlConnection connection = new SqlConnection(connectionString)) {
+            connection.Open();
+
+            string sql = "SELECT * FROM Users";
+            SqlCommand command = new SqlCommand(sql, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            if(reader.HasRows) {
+                Console.Write("woo");
+            }
+            connection.Close();
+        }
+
         var user1 =  new User{Name = "Bobby", Email = "Hey", MembershipType = "Yes"};
         _users.Add(user1);
         return _users;
