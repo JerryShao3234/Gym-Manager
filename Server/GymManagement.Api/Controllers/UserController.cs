1e11ed8f39cc98618d1d880f7508921403f4c47b
JerryShao3234 <shaojerry@icloud.com>
2023-03-05 16:55:16 -0800

    Integrate GetAll of /users with db

--- Server/GymManagement.Api/Controllers/UserController.cs
+++ Server/GymManagement.Api/Controllers/UserController.cs
@@ -8,21 +8,25 @@ namespace GymManagement.Api.Controllers;
 [ApiController]
 [Route("user/")]
 
-public class UserController : ControllerBase{
+public class UserController : ControllerBase
+{
     private readonly IUserService _userService;
-    public UserController(IUserService userService) {
+    public UserController(IUserService userService)
+    {
         _userService = userService;
     }
 
     [HttpPost("register")]
 
-    public IActionResult Register(RegisterRequest request) {
+    public IActionResult Register(RegisterRequest request)
+    {
         var registerResult = _userService.Register(request.Name, request.Email, request.MembershipType);
         var response = new RegisterResult(registerResult.Name, registerResult.Email, registerResult.MembershipType);
         return Ok(response);
     }
 
-    public OkObjectResult GetAll() {
+    public OkObjectResult GetAll()
+    {
         var response = _userService.GetAll();
         return Ok(response);
     }
