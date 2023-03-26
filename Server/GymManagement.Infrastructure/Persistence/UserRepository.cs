4a82a44134204e88e677dc7d4b5e4204fdf116e9
JerryShao3234 <shaojerry@icloud.com>
2023-03-26 13:29:56 -0700

    Integrate Users routes with database

--- Server/GymManagement.Infrastructure/Persistence/UserRepository.cs
+++ Server/GymManagement.Infrastructure/Persistence/UserRepository.cs
@@ -7,7 +7,6 @@ namespace GymManagement.Infrastructure.Persistence;
 
 public class UserRepository : IUserRepository
 {
-    private static readonly List<User> _users = new();
     private IConfiguration _config;
 
     public UserRepository(IConfiguration config) {
@@ -16,20 +15,59 @@ public class UserRepository : IUserRepository
     public void Add(User user)
     {
         Console.WriteLine("Adding user " + user.Email);
-        _users.Add(user);
+
+        string connectionString = "Data Source=localhost;Initial Catalog=Tutorial2;Integrated Security=True";
+        using (SqlConnection connection = new SqlConnection(connectionString)) {
+            connection.Open();
+
+            string sql = "INSERT INTO Users (Name, Email, MembershipType) VALUES (@Name, @Email, @MembershipType)";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Name", user.Name);
+            command.Parameters.AddWithValue("@Email", user.Email);
+            command.Parameters.AddWithValue("@MembershipType", user.MembershipType);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
     }
     public void Delete(User user)
     {
         Console.WriteLine("Deleting user " + user.Email);
-        _users.Remove(user);
+
+        string connectionString = "Data Source=localhost;Initial Catalog=Tutorial2;Integrated Security=True";
+        using (SqlConnection connection = new SqlConnection(connectionString)) {
+            connection.Open();
+
+            string sql = "DELETE FROM Users WHERE Email = @Email";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Email", user.Email);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
     }
 
     public User? GetUserByEmail(string email)
     {
-        return _users.SingleOrDefault(u => u.Email == email);
+        string connectionString = "Data Source=localhost;Initial Catalog=Tutorial2;Integrated Security=True";
+        using (SqlConnection connection = new SqlConnection(connectionString)) {
+            connection.Open();
+
+            string sql = "SELECT * FROM Users WHERE Email = @Email";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Email", email);
+            SqlDataReader reader = command.ExecuteReader();
+            Console.WriteLine("Reading data from table");
+            while (reader.Read()) {
+                var user = new User{Name = reader["Name"].ToString() ?? "", Email = reader["Email"].ToString() ?? "", MembershipType = reader["MembershipType"].ToString() ?? ""};
+                return user;
+            }
+            connection.Close();
+        }
+
+        return null;
     }
 
     public List<User> GetAll () {
+        List<User> _users = new List<User>();
         string connectionString = "Data Source=localhost;Initial Catalog=Tutorial2;Integrated Security=True";
             using (SqlConnection connection = new SqlConnection(connectionString)) {
             connection.Open();
