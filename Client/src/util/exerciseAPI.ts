1f11ab9319a75024f8d82ffc3b9466a078c4becf
dashdot <30674558+Graywing13@users.noreply.github.com>
2023-04-04 18:10:26 -0700

    Update exerciseAPI.ts to be like main

--- Client/src/util/exerciseAPI.ts
+++ Client/src/util/exerciseAPI.ts
@@ -4,7 +4,6 @@ import { BASE_URL, getErrorMessage, TableEntry, unwrapResponse } from "./rest";
 export async function getExercises(): Promise<TableEntry[]> {
   try {
     const response = await axios.get(BASE_URL + "exercise");
-    // console.log(response)
     return unwrapResponse(response);
   } catch (err: any) {
     throw new Error(getErrorMessage(err));
@@ -16,6 +15,16 @@ export async function deleteExercise(
 ): Promise<TableEntry[]> {
   try {
     const response = await axios.delete(BASE_URL + "exercise/" + exerciseName);
+    return unwrapResponse(response);
+  } catch (err: any) {
+    throw new Error(getErrorMessage(err));
+  }
+}
+
+export async function getExercisesThatTargetAll(): Promise<TableEntry[]> {
+  try {
+    const response = await axios.get(BASE_URL + "exercise/targetAll");
+    console.log(response);
     return unwrapResponse(response);
   } catch (err: any) {
     throw new Error(getErrorMessage(err));
